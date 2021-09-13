263078746a727c5a6658ae4d0376d12d9397fbf8
ac24 <ac24@CIS-RDS3>
2021-09-13 14:39:49 -0500

    Committing changes accidentally made on ms2 branch

--- Data/Entrees/Gyro.cs
+++ Data/Entrees/Gyro.cs
@@ -1,11 +1,22 @@
-﻿using System;
+﻿/*
+ * Gyro.cs
+ * Author: Alisha C.
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/// <summary>
+/// Entrees
+/// </summary>
 namespace GyroScope.Data.Entrees
 {
+    /// <summary>
+    /// A base class for all Gyros sold at GyroScope
+    /// </summary>
     public abstract class Gyro
     {
 
