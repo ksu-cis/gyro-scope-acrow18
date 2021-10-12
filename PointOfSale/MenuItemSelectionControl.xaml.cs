3dc1e43b71090c146d9adb0607fc45a2223c458f
ac24 <ac24@CIS-RDS3>
2021-10-11 23:11:44 -0500

    Menu Item switch screen done

--- PointOfSale/MenuItemSelectionControl.xaml.cs
+++ PointOfSale/MenuItemSelectionControl.xaml.cs
@@ -24,5 +24,78 @@ namespace PointOfSale
         {
             InitializeComponent();
         }
+
+        /// <summary>
+        /// Event in button being clicked
+        /// </summary>
+        void ButtonClicked(object sender, RoutedEventArgs e) 
+        {
+            if (sender is Button button) 
+            {
+
+            }
+        }
+
+        /// <summary>
+        /// Switches to correct customization menu
+        /// </summary>
+        /// <param name="name"></param>
+        public void SwitchScreen(string name) 
+        {
+            switch (name) 
+            {
+                ///Drinks
+                case "Capricorn Mountain Tea":
+                    menuSelectionControl.Child = new CapricornMountainTeaControl();
+                break;
+
+                case "Libra Libation":
+                    menuSelectionControl.Child = new LibraLibation();
+                break;
+
+                ///Entrees
+                case "Virgo Classic Gyro":
+                    menuSelectionControl.Child = new GyroCustomizationControl();
+                break;
+
+                case "Leo Lamb Gyro":
+                    menuSelectionControl.Child = new GyroCustomizationControl();
+                break;
+
+                case "Scorpio Spicy Gyro":
+                    menuSelectionControl.Child = new GyroCustomizationControl();
+                break;
+
+                case "Pisces Fish Dish":
+                    menuSelectionControl.Child = new PiscesFishDishControl();
+                break;
+
+                ///Sides
+                case "Aries Fries":
+                    menuSelectionControl.Child = new SidesControl();
+                break;
+
+                case "Gemini Stuffed Grape Leaves":
+                    menuSelectionControl.Child = new SidesControl();
+                break;
+
+                case "Sagittarius Greek Salad":
+                    menuSelectionControl.Child = new SidesControl();
+                break;
+
+                case "Taurus Tabuleh":
+                    menuSelectionControl.Child = new SidesControl();
+                break;
+
+                ///Treats
+                case "Aquarius Ice":
+                    menuSelectionControl.Child = new AquariusIceControl();
+                break;
+
+                case "Cancer Halva Cake":
+                    menuSelectionControl.Child = new CancerHalvaCakeControl();
+                break;
+            }
+        }
     }
 }
