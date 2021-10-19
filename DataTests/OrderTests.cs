00bbf56cead4a50e8c7b2ec055bfdae47475cc84
Alisha Crow <ac24@users.campus>
2021-10-18 22:27:07 -0500

    Save

--- DataTests/OrderTests.cs
+++ DataTests/OrderTests.cs
@@ -4,8 +4,10 @@
  */
 
 using GyroScope.Data;
+using GyroScope.Data.Entrees;
 using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,7 +20,57 @@ namespace GyroScope.DataTests
     /// </summary>
     public class OrderTests
     {
-        
+        /// <summary>
+        /// Notifies of collection changed on add
+        /// </summary>
+        [Fact]
+        public void ShouldNotifyOfCollectionChangedOnAdd()
+        {
+            var order = new Order<IMenuItem>();
+            NotifyCollectionChangedEventArgs args = null;
+
+            order.CollectionChanged += (sender, e) =>
+            {
+                args = e;
+            };
+
+            var leoLambGyro = new LeoLambGyro();
+            order.Add(leoLambGyro);
+            Assert.NotNull(args);
+            Assert.Equal(NotifyCollectionChangedAction.Add, args.Action);
+            Assert.Equal(order, args.NewItems[0]);
+            Assert.Equal(1, args.NewItems.Count);
+            Assert.Null(args.OldItems);
+        }
+
+        /// <summary>
+        /// Notifies of collection changed on add
+        /// </summary>
+        [Fact]
+        public void ShouldNotifyOfCollectionChangedORemove()
+        {
+            var order = new Order<IMenuItem>();
+            NotifyCollectionChangedEventArgs args = null;
+
+            order.CollectionChanged += (sender, e) =>
+            {
+                args = e;
+            };
+
+            var leoLambGyro = new LeoLambGyro();
+            var scorpioSpicyGyro = new ScorpioSpicyGyro();
+
+            order.Add(leoLambGyro);
+            order.Add(scorpioSpicyGyro);
+
+            order.Remove(leoLambGyro, 0);
+            Assert.NotNull(args);
+            Assert.Equal(NotifyCollectionChangedAction.Remove, args.Action);
+            Assert.Equal(order, args.NewItems[0]);
+            Assert.Equal(1, args.NewItems.Count);
+            Assert.Null(args.OldItems);
+        }
+
         /// <summary>
         /// Checks if INotifyCollectionChanged is implemented
         /// </summary>
