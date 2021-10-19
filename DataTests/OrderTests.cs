3716907f0d28be9c45df2879e11bec032e76e9a4
Alisha Crow <ac24@users.campus>
2021-10-18 23:17:53 -0500

    Save to work on tomorrow

--- DataTests/OrderTests.cs
+++ DataTests/OrderTests.cs
@@ -40,6 +40,7 @@ namespace GyroScope.DataTests
             Assert.Equal(NotifyCollectionChangedAction.Add, args.Action);
             Assert.Equal(order, args.NewItems[0]);
             Assert.Equal(1, args.NewItems.Count);
+            Assert.Null(args.OldItems);
         }
 
         /// <summary>
@@ -59,10 +60,12 @@ namespace GyroScope.DataTests
             var leoLambGyro = new LeoLambGyro();
             var scorpioSpicyGyro = new ScorpioSpicyGyro();
 
+            ///adding items to be able to remove an item
             order.Add(leoLambGyro);
             order.Add(scorpioSpicyGyro);
 
             order.Remove(leoLambGyro, 0);
+
             Assert.NotNull(args);
             Assert.Equal(NotifyCollectionChangedAction.Remove, args.Action);
             Assert.Equal(order, args.NewItems[0]);
