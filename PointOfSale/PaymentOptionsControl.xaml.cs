c477cc80164610a3b6fef82b3c819c3696bd6290
ac24 <ac24@CIS-RDS3>
2021-10-28 17:31:13 -0500

    Save

--- PointOfSale/PaymentOptionsControl.xaml.cs
+++ PointOfSale/PaymentOptionsControl.xaml.cs
@@ -40,18 +40,18 @@ namespace PointOfSale
         /// Event handler for "Debit" button click
         /// </summary>
         /// <param name="sender">sender</param>
-        /// <param name="e"></param>
+        /// <param name="e">e</param>
         private void debitButton_Click(object sender, RoutedEventArgs e)
         {
-            Order order = new Order();
-            double total = (double)order.Total;
+            MainWindow main = new MainWindow();
+            double total = (double)main.Order.Total;
             if (RoundRegister.CardReader.RunCard(total) == RoundRegister.CardTransactionResult.Approved)
             {
                 //print recipt
                 Order newOrder = new Order();
             }
 
-            else if (RoundRegister.CardReader.RunCard(total) == RoundRegister.CardTransactionResult.Declined) 
+            else if (RoundRegister.CardReader.RunCard(total) == RoundRegister.CardTransactionResult.Declined)
             {
                 MessageBox.Show("This card is declined");
                 //try a different card
@@ -80,11 +80,40 @@ namespace PointOfSale
         /// <summary>
         /// Event handler for "Credit" button click
         /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
+        /// <param name="sender">sender</param>
+        /// <param name="e">e</param>
         private void creditButton_Click(object sender, RoutedEventArgs e)
         {
+            MainWindow main = new MainWindow();
+            double total = (double)main.Order.Total;
+            if (RoundRegister.CardReader.RunCard(total) == RoundRegister.CardTransactionResult.Approved)
+            {
+                //print recipt
+                Order newOrder = new Order();
+            }
 
+            else if (RoundRegister.CardReader.RunCard(total) == RoundRegister.CardTransactionResult.Declined)
+            {
+                MessageBox.Show("This card is declined");
+                //try a different card
+            }
+
+            else if (RoundRegister.CardReader.RunCard(total) == RoundRegister.CardTransactionResult.ReadError)
+            {
+                MessageBox.Show("This card has a read error");
+                //have them swipe card again
+            }
+
+            else if (RoundRegister.CardReader.RunCard(total) == RoundRegister.CardTransactionResult.InsufficientFunds)
+            {
+                MessageBox.Show("This card has insufficient funds");
+            }
+
+            else if (RoundRegister.CardReader.RunCard(total) == RoundRegister.CardTransactionResult.IncorrectPin)
+            {
+                MessageBox.Show("This card's pin is incorrect");
+                //if there was a correct pin enum test again
+            }
         }
     }
 }
