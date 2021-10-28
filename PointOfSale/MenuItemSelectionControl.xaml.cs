b55d2033ce43631b5b3fa7bd3ba9571a048b24ee
ac24 <ac24@CIS-RDS3>
2021-10-28 17:05:44 -0500

    Save to go back to MS7

--- PointOfSale/MenuItemSelectionControl.xaml.cs
+++ PointOfSale/MenuItemSelectionControl.xaml.cs
@@ -16,6 +16,7 @@ using GyroScope.Data.Drinks;
 using GyroScope.Data.Entrees;
 using GyroScope.Data.Treats;
 using GyroScope.Data.Sides;
+using GyroScope.Data;
 
 namespace PointOfSale
 {
@@ -34,18 +35,18 @@ namespace PointOfSale
         /// </summary>
         /// <param name="sender">Sender</param>
         /// <returns>Content on button</returns>
-        public string ItemNameOfButtonClicked(object sender) 
+        public string ItemNameOfButtonClicked(object sender)
         {
             if (sender is Button button)
             {
                 return button.Content.ToString();
             }
 
-            else 
+            else
             {
                 throw new InvalidOperationException("Item doesn't exist");
             }
-            
+
         }
 
         /// <summary>
@@ -53,100 +54,120 @@ namespace PointOfSale
         /// </summary>
         /// <param name="name">Name of menu item</param>
         /// <param name="sender">Sender</param>
-        public void SwitchScreen(string name) 
+        public void SwitchScreen(string name)
         {
             ///not a var because vars need to be set to something
             dynamic customization;
+            MainWindow mainWindow = FindMainWindow();
+            Border orderBorder = (Border)mainWindow.orderSummary;
+            OrderSummaryControl orderSumControl = (OrderSummaryControl)orderBorder.Child;
+            Order order = (Order)orderSumControl.DataContext;
 
-            switch (name) 
+            switch (name)
             {
                 ///Drinks
                 case "Capricorn Mountain Tea":
                     customization = new CapricornMountainTeaControl();
                     MenuSelectionControl.Child = customization;
                     CapricornMountainTea capricornMountainTea = new CapricornMountainTea();
-                    customization.DataContext = capricornMountainTea; 
-                break;
+                    order.Add(capricornMountainTea);
+                    customization.DataContext = capricornMountainTea;
+                    break;
 
                 case "Libra Libation":
                     customization = new LibraLibationControl();
                     MenuSelectionControl.Child = customization;
                     LibraLibation libraLibation = new LibraLibation();
+                    order.Add(libraLibation);
                     customization.DataContext = libraLibation;
-                break;
+                    break;
 
                 ///Entrees
                 case "Virgo Classic Gyro":
                     customization = new GyroCustomizationControl();
+                    customization.NameOfGyroTextBlock.Text = "Customize Virgo Classic Gyro";
                     MenuSelectionControl.Child = customization;
                     VirgoClassicGyro virgoClassicGyro = new VirgoClassicGyro();
+                    order.Add(virgoClassicGyro);
                     customization.DataContext = virgoClassicGyro;
-                break;
+                    break;
 
                 case "Leo Lamb Gyro":
                     customization = new GyroCustomizationControl();
+                    customization.NameOfGyroTextBlock.Text = "Customize Leo Lamb Gyro";
                     MenuSelectionControl.Child = customization;
                     LeoLambGyro leoLambGyro = new LeoLambGyro();
+                    order.Add(leoLambGyro);
                     customization.DataContext = leoLambGyro;
-                break;
+                    break;
 
                 case "Scorpio Spicy Gyro":
                     customization = new GyroCustomizationControl();
+                    customization.NameOfGyroTextBlock.Text = "Customize Scorpio Spicy Gyro";
                     MenuSelectionControl.Child = customization;
                     ScorpioSpicyGyro scorpioSpicyGyro = new ScorpioSpicyGyro();
+                    order.Add(scorpioSpicyGyro);
                     customization.DataContext = scorpioSpicyGyro;
-                break;
+                    break;
 
                 case "Pisces Fish Dish":
                     customization = new PiscesFishDishControl();
                     MenuSelectionControl.Child = customization;
                     PiscesFishDish piscesFishDish = new PiscesFishDish();
+                    order.Add(piscesFishDish);
                     customization.DataContext = piscesFishDish;
-                break;
+                    break;
 
                 ///Sides
                 case "Aries Fries":
                     customization = new SidesControl();
+
                     MenuSelectionControl.Child = customization;
                     AriesFries ariesFries = new AriesFries();
+                    order.Add(ariesFries);
                     customization.DataContext = ariesFries;
-                break;
+                    break;
 
                 case "Gemini Stuffed Grape Leaves":
                     customization = new SidesControl();
                     MenuSelectionControl.Child = customization;
                     GeminiStuffedGrapeLeaves geminiStuffedGrapeLeaves = new GeminiStuffedGrapeLeaves();
+                    order.Add(geminiStuffedGrapeLeaves);
                     customization.DataContext = geminiStuffedGrapeLeaves;
-                break;
+                    break;
 
                 case "Sagittarius Greek Salad":
                     customization = new SidesControl();
                     MenuSelectionControl.Child = customization;
                     SagittariusGreekSalad sagittariusGreekSalad = new SagittariusGreekSalad();
+                    order.Add(sagittariusGreekSalad);
                     customization.DataContext = sagittariusGreekSalad;
-                break;
+                    break;
 
                 case "Taurus Tabuleh":
                     customization = new SidesControl();
                     MenuSelectionControl.Child = customization;
                     TaurusTabuleh taurusTabuleh = new TaurusTabuleh();
+                    order.Add(taurusTabuleh);
                     customization.DataContext = taurusTabuleh;
-                break;
+                    break;
 
                 ///Treats
                 case "Aquarius Ice":
                     customization = new AquariusIceControl();
                     MenuSelectionControl.Child = customization;
                     AquariusIce aquariusIce = new AquariusIce();
+                    order.Add(aquariusIce);
                     customization.DataContext = aquariusIce;
-                break;
+                    break;
 
                 case "Cancer Halva Cake":
                     customization = new CancerHalvaCakeControl();
                     MenuSelectionControl.Child = customization;
                     CancerHalvaCake cancerHalvaCake = new CancerHalvaCake();
+                    order.Add(cancerHalvaCake);
                     customization.DataContext = cancerHalvaCake;
-                break;
+                    break;
             }
         }
 
@@ -162,6 +183,25 @@ namespace PointOfSale
                 SwitchScreen(button.Content.ToString());
                 e.Handled = true;
             }
+        }
+
+        /// <summary>
+        /// Finds the main window
+        /// </summary>
+        /// <returns>A main window</returns>
+        public MainWindow FindMainWindow()
+        {
+
+            DependencyObject parent = this;
+
+            do
+            {
+                parent = LogicalTreeHelper.GetParent(parent);
+            }
+
+            while (!(parent is null || parent is MainWindow));
+            MainWindow mainWindow = (MainWindow)parent;
+            return mainWindow;
         }
     }
 }
