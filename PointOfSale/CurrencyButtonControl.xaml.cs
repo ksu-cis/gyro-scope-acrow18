117b15cf0c4b310d9e1d36691072a7bd535ca9d5
ac24 <ac24@CIS-RDS3>
2021-10-31 16:01:44 -0500

    Save

--- PointOfSale/CurrencyButtonControl.xaml.cs
+++ PointOfSale/CurrencyButtonControl.xaml.cs
@@ -25,20 +25,67 @@ namespace PointOfSale
             InitializeComponent();
         }
 
-
+        /// <summary>
+        /// Dependency property representing amount to give as change
+        /// </summary>
+        public static DependencyProperty OutgoingCountProperty = DependencyProperty.Register("OutgoingCount", typeof(int), typeof(CurrencyButtonControl));
 
         /// <summary>
-        /// Creates a property for the plus and minus button
+        /// Dependency property representing amount coming from customer
         /// </summary>
-        public static readonly DependencyProperty StepProperty = DependencyProperty.Register("Step", typeof(double), typeof(CurrencyButtonControl), new PropertyMetadata(1.0));
+        public static DependencyProperty IncomingCountProperty = DependencyProperty.Register("IncomingCount", typeof(int), typeof(CurrencyButtonControl));
+
+        public static readonly IncomingClickChangeEvent = EventManager.RegisterRoutedEvent("IncomingChangeClick", RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(CurrencyButtonControl));
 
         /// <summary>
-        /// The amount each increment or decrement operation should change the value by
+        /// Number of denomination given back as change.
         /// </summary>
-        public double Step
+        public int OutgoingCount
         {
-            get { return (double)GetValue(StepProperty); }
-            set { SetValue(StepProperty, value); }
+            get { return (int)GetValue(OutgoingCountProperty); }
+            set { SetValue(OutgoingCountProperty, value);  }
+        }
+
+        /// <summary>
+        /// Number of denomination being recived by the cashier.
+        /// </summary>
+        public int IncomingCount
+        {
+            get { return (int)GetValue(IncomingCountProperty); }
+            set { SetValue(IncomingCountProperty, value); }
+        }
+
+        /// <summary>
+        /// Event handler to change the incoming bill count.
+        /// </summary>
+        public event RoutedEventHandler IncomingChangeClick 
+        {
+            add { AddHandler(IncomingChangeClickEvent, value); }
+            remove { RemoveHandler(IncomingChangeClickEvent, value); }
+        }
+
+
+        /// <summary>
+        /// Handles the even that updates the count of the incoming bills
+        /// </summary>
+        /// <param name="sender">sender</param>
+        /// <param name="e">e</param>
+        public void HandleIncomingClick(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button button) 
+            {
+                switch (button.Name) 
+                {
+                    case "plusButton":
+                        IncomingCount++;
+                        break;
+
+                    case "minusButton":
+                        if(IncomingCount > 0) 
+                        IncomingCount--;
+                        break;
+                }
+            }
         }
     }
 }
