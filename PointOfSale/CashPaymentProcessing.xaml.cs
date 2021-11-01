6209b8371534de0815c17734c2195dfb473f6077
ac24 <ac24@CIS-RDS3>
2021-10-31 20:00:43 -0500

    Save

--- PointOfSale/CashPaymentProcessing.xaml.cs
+++ PointOfSale/CashPaymentProcessing.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GyroScope.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,7 @@ namespace PointOfSale
         public CashPaymentProcessing()
         {
             InitializeComponent();
+            this.DataContext = new RegisterViewModel();
         }
 
         /// <summary>
