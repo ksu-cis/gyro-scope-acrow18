1582bc1180d9980610ebbe23fca38e79ba6c5905
ac24 <ac24@CIS-RDS3>
2021-11-02 16:52:32 -0500

    save

--- DataTests/RegisterViewModelTests.cs
+++ DataTests/RegisterViewModelTests.cs
@@ -14,6 +14,9 @@ namespace GyroScope.DataTests
     /// </summary>
     public class RegisterViewModelTests
     {
+        /// <summary>
+        /// Checks if change owed is correct
+        /// </summary>
         [Fact]
         public void IsChangeOwedCorrect()
         {
@@ -21,11 +24,15 @@ namespace GyroScope.DataTests
             order.Add(new ScorpioSpicyGyro());
             order.Add(new VirgoClassicGyro());
             order.Add(new VirgoClassicGyro());
-            var viewModel = new RegisterViewModel(order);
-            viewModel.CustomerHundreds = 1;
-            Assert.Equal(viewModel.ChangeOwed, viewModel.Total - viewModel.Customer);
+            var RVM= new RegisterViewModel(order);
+            RVM.CustomerHundreds = 1;
+            Assert.Equal(RVM.ChangeOwed, RVM.Total - RVM.Customer);
         }
 
+
+        /// <summary>
+        /// Checks if amount due is correct
+        /// </summary>
         [Fact]
         public void IsAmountDueCorrect()
         {
@@ -33,17 +40,69 @@ namespace GyroScope.DataTests
             order.Add(new ScorpioSpicyGyro());
             order.Add(new VirgoClassicGyro());
             order.Add(new VirgoClassicGyro());
-            var viewModel = new RegisterViewModel(order);
-            viewModel.CustomerHundreds = 1;
-            Assert.Equal(viewModel.AmountDue, viewModel.Total - viewModel.Customer);
+            var RVM = new RegisterViewModel(order);
+            RVM.CustomerHundreds = 1;
+            Assert.Equal(RVM.AmountDue, RVM.Total - RVM.Customer);
         }
 
-        /*
+        
+        /// <summary>
+        /// Change owed should default to zero
+        /// </summary>
         [Fact]
-        public void IsCashRegisterCorrect()
+        public void ChangeOwedShouldDefaultToZero()
         {
-            var cashRegister
+            var order = new Order();
+            order.Add(new VirgoClassicGyro());
+            var RVM = new RegisterViewModel(order);
+            Assert.Equal(0, RVM.ChangeOwed);
         }
-        */
+
+
+        /// <summary>
+        /// Total due should equal the the total of the order
+        /// </summary>
+        [Fact]
+        public void TotalDueShouldEqualOrderCost()
+        {
+            var order = new Order();
+            order.Add(new VirgoClassicGyro());
+            var RVM = new RegisterViewModel(order);
+            Assert.Equal(RVM.Total, order.Total);
+        }
+
+        /// <summary>
+        /// Change owed to customer should be calculated correctly
+        /// </summary>
+        /// <param name="num">Number of twenty dollar bills recieved from customer</param>
+        /// <param name="total">Total remaining from customer</param>
+        [Theory]
+        [InlineData(1, 14)]
+        [InlineData(2, 8)]
+        [InlineData(3, 2)]
+        public void ChangeOwedShouldCalculateCorrectly(int num, decimal total) 
+        {
+            var order = new Order();
+            order.Add(new VirgoClassicGyro());
+            var RVM = new RegisterViewModel(order);
+            RVM.CustomerTwenties = num;
+            Assert.Equal(total, RVM.ChangeOwed);
+        }
+
+
+        [Theory]
+        [InlineData()]
+        public void MoneyShouldBeRemovedFromCashRegister(int customerBills, int drawerTwos, int drawerTens) 
+        {
+            var order = new Order();
+            order.Add(new VirgoClassicGyro());
+            var RVM = new RegisterViewModel(order);
+            RVM.CustomerTwos = customerBills;
+            Assert.Equal(drawerTwos, RVM.CashDrawerTwos);
+            Assert.Equal(drawerTens, RVM.CashDrawerTens);
+
+        }
+
+
     }
 }
