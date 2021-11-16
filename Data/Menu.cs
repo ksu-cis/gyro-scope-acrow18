2471410c188a2c86672e29cc057b391e171cd132
ac24 <ac24@CIS-RDS3>
2021-11-16 15:05:25 -0600

    Partial done with menu class

--- Data/Menu.cs
+++ Data/Menu.cs
@@ -2,6 +2,9 @@
  * Menu.cs
  * Author: ALisha C.
  */
+using GyroScope.Data.Entrees;
+using GyroScope.Data.Enums;
+using GyroScope.Data.Sides;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,26 +22,53 @@ namespace GyroScope.Data
         /// <summary>
         /// Gets a list of the entrees
         /// </summary>
-        public static IEnumerable<IMenuItem> Entrees { get; set; }
+        public static IEnumerable<IMenuItem> Entrees 
+        { 
+            get 
+            {
+                var entrees = new List<IMenuItem>();
+                entrees.Add(new VirgoClassicGyro());
+                entrees.Add(new LeoLambGyro());
+                entrees.Add(new ScorpioSpicyGyro());
+                entrees.Add(new PiscesFishDish());
+                return entrees;
+            }
+        }
 
         /// <summary>
         /// Gets a list of the sides
         /// </summary>
-        public static IEnumerable<IMenuItem> Sides{ get; set; }
+        public static IEnumerable<IMenuItem> Sides
+        {
+            //change foreach
+            get
+            {
+                var sides = new List<IMenuItem>();
+                foreach (Size size in Enum.GetValues(typeof(Size)))
+                {
+                    sides.Add(new AriesFries() { Size = size });
+                    sides.Add(new GeminiStuffedGrapeLeaves() { Size = size });
+                    sides.Add(new SagittariusGreekSalad() { Size = size });
+                    sides.Add(new TaurusTabuleh() { Size = size });
+                }
+
+                return sides;
+            }
+        }
 
         /// <summary>
         /// Gets a list of drinks 
         /// </summary>
-        public static IEnumerable<IMenuItem> Drinks { get; set; }
+        public static IEnumerable<IMenuItem> Drinks { get;  }
 
         /// <summary>
         /// Gets a list of drinks 
         /// </summary>
-        public static IEnumerable<IMenuItem> Treats { get; set; }
+        public static IEnumerable<IMenuItem> Treats { get;  }
 
         /// <summary>
         /// Gets a full menu list of all gyro scope options
         /// </summary>
-        public static IEnumerable<IMenuItem> FullMenu { get; set; }
+        public static IEnumerable<IMenuItem> FullMenu { get; }
     }
 }
